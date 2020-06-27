62f2bff45f3c144d5178a4317367cb09f4044451
Vipul Rao <svipulrao98@gmail.com>
2020-06-27 17:09:35 +0530

    fetching from database & dark, light mode

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
-import React, { Component } from "react";
+import React,{useState, useEffect} from "react";
 import "./App.css";
 import ChatBot from "./Components/chatbot/ChatBot"
 import firebase from './Components/firebase';
 import ReactSpinner from 'react-bootstrap-spinner';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
-class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          steps_: []
-      }
-  };
+function App () {
+     
+  const [steps_, setStep] = useState([])
+  const [mode, setMode] = useState("grey")
+  const [variant, setVariant] = useState(["outline-secondary", "Light Mode"])
 
-  componentDidMount() {
-      const eventRef=firebase.database().ref('events');
-      eventRef.on('value', (snapshot) => {
-          let events = snapshot.val();
-          let newState=[];
-          for(let eve in events){
-              newState.push(events[eve])
-          }
-          this.setState({
-            steps_: newState
-          });
-        });
-  }
-  render() {
-    if(this.state.steps_.length===0){
-      // console.log('Still loading');
+  useEffect(() => {
+    const eventRef=firebase.database().ref('events');
+      const mounted = eventRef.on('value', (snapshot) => {
+        let events = snapshot.val();
+        setStep(events);
+      });
+     
+    return () => {
+      mounted()
+    }},[])
+    if(steps_.length===0){
       return (
         <div>
           <Row><br /><br /><br /><br /><br /><br /><br /><br /></Row>
           <Row>
-            <Col></Col>
-            <Col></Col>
+            <Col></Col><Col></Col>
             <Col>
               <ReactSpinner type="grow" color="primary" size="7" />
             </Col>
-            <Col></Col>
-            <Col></Col>
+            <Col></Col><Col></Col>
           </Row>
           <Row></Row>
         </div>
       )
     }
     else {
-      // console.log(this.state.steps_);
-      return <ChatBot steps={this.state.steps_} />;
+      return (
+        <div>
+          <Button 
+            variant={variant[0]} 
+            size="lg" 
+            onClick={() => mode === "grey" ? 
+              (setMode("white"), setVariant(["secondary", "Dark Mode"])) : 
+              (setMode("grey"), setVariant(["outline-secondary", "Ligth Mode"])) 
+              } block>{variant[1]}</Button>
+          <br /><ChatBot steps={steps_} mode = {mode}/>
+        </div>
+      );
     }
   }
-}
+
 
 export default App;
--- src/Components/chatbot/ChatBot.js
+++ src/Components/chatbot/ChatBot.js
@@ -3,29 +3,30 @@ import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from "styled-components";
 
 function CustomChatbot(props) {
-    const config = {
-      width: "100%",
-      height: "100%",
-      // floating: true
-    };
+  const config = {
+    width: "100%",
+    height: "100%",
+    // floating: true
+  };
+  const theme = {
+    background: props.mode,
+    fontFamily: "Arial, Helvetica, sans-serif",
+    headerBgColor: "#00B2B2",
+    headerFontColor: "#fff",
+    headerFontSize: "25px",
+    botBubbleColor: "#00B2B2",
+    botFontColor: "#fff",
+    userBubbleColor: "#fff",
+    userFontColor: "#4c4c4c",
+  };
 
-    const theme = {
-      background: "white",
-      fontFamily: "Arial, Helvetica, sans-serif",
-      headerBgColor: "#00B2B2",
-      headerFontColor: "#fff",
-      headerFontSize: "25px",
-      botBubbleColor: "#00B2B2",
-      botFontColor: "#fff",
-      userBubbleColor: "#fff",
-      userFontColor: "#4c4c4c"
-    };
-    const steps=props.steps;
-    return (
-        <ThemeProvider theme={theme}>
-          <ChatBot steps={steps} {...config} />
-        </ThemeProvider>
-    );
+  const steps=props.steps.events;
+  console.log(steps);
+  return (
+    <ThemeProvider theme={theme}>
+      <ChatBot steps={steps} {...config} />
+    </ThemeProvider>
+  );
 }
 
 export default CustomChatbot;
